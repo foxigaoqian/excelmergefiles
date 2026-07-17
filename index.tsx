3fb968a56c2dea55a2f4dffd99e71a9681d2bd84
foxigaoqian <foxigaoqian@gmail.com>
2026-07-17 12:53:09 +0800

    Load route-specific spreadsheet tool

--- index.tsx
+++ index.tsx
@@ -1,14 +1,16 @@
-
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+import ToolApp from './ToolApp';
+import { MergeMode, ToolType } from './types';
 
 const rootElement = document.getElementById('root');
-if (!rootElement) throw new Error("Could not find root element to mount to");
+if (!rootElement) throw new Error('Could not find root element to mount to');
 
-const root = ReactDOM.createRoot(rootElement);
-root.render(
+const initialTool = (rootElement.dataset.tool || 'merge') as ToolType;
+const initialMergeMode = (rootElement.dataset.mergeMode || 'append-rows') as MergeMode;
+
+ReactDOM.createRoot(rootElement).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <ToolApp initialTool={initialTool} initialMergeMode={initialMergeMode} />
+  </React.StrictMode>,
 );
