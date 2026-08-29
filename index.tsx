4e0440d3ce7730a67e2af8a25c83bd318e5bccbd
foxigaoqian <taa1@foxmail.com>
2026-08-29 14:25:42 +0800

    Load compiled production styles

--- index.tsx
+++ index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import './styles.css';
 import ToolApp from './ToolApp';
 import SeoContent from './SeoContent';
 import { MergeMode, ToolType } from './types';
